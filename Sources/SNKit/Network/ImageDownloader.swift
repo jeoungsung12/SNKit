d0386d19a7bc7346eb1d770f8b3206c10624f873
jeoungsung12 <jeoungsung12@naver.com>
2025-04-02 17:23:24 +0900

    [Fix] ImageDownloader/ETagHandler Error 및 결과 처리 수정

--- Sources/SNKit/Network/ImageDownloader.swift
+++ Sources/SNKit/Network/ImageDownloader.swift
@@ -13,10 +13,25 @@ public enum CacheOption {
     case forceDownload
 }
 
+public enum DownloadResult {
+    case success(UIImage)
+    case cached(UIImage)
+    case validated(UIImage)
+    case failure(Error)
+}
+
+public enum DownloadError: Error {
+    case invalidData
+    case networkError(Error)
+    case invalidResponse
+    case imageProcessingFailed
+}
+
 final class ImageDownloader: @unchecked Sendable {
     private let session: URLSession
     private let cacheManager: CacheManager
     private let eTagHandler: ETagHandler
+    private let dispatchQueue = DispatchQueue(label: "com.snkit.imagedownloader", qos: .utility, attributes: .concurrent)
     
     init(
         session: URLSession,
@@ -25,30 +40,33 @@ final class ImageDownloader: @unchecked Sendable {
         self.session = session
         self.cacheManager = cacheManager
         self.eTagHandler = ETagHandler(session: session, cacheManager: cacheManager)
-        
     }
     
     func downloadImage(
         with url: URL,
         option: CacheOption = .cacheFirst ,
-        completion: @escaping @Sendable (UIImage?) -> Void
+        completion: @escaping @Sendable (DownloadResult?) -> Void
     ) {
         //캐시 확인
         let identifier = url.absoluteString
+        
         switch option {
         case .cacheFirst:
             //캐시 Hit -> 캐시에 저장된 이미지 반환
             if let cachedImage = cacheManager.retrieveImage(with: identifier) {
-                completion(cachedImage)
+                DispatchQueue.main.async {
+                    completion(.cached(cachedImage))
+                }
                 return
             }
             downloadAndCacheImage(with: url, identifier: identifier, completion: completion)
+            
         case .eTagValidation:
             //TODO: Etag 검증, 캐시 Hit -> Etag 확인, 없으면 그냥 다운
             if let cachedImage = cacheManager.retrieveImage(with: identifier),
                let cachedETag = cacheManager.retrieveETag(with: identifier) {
-                eTagHandler.validateFetchImage(
-                    with: url,
+                validateWithETag(
+                    url: url,
                     cachedImage: cachedImage,
                     cachedETag: cachedETag,
                     completion: completion
@@ -62,47 +80,108 @@ final class ImageDownloader: @unchecked Sendable {
         }
     }
     
+}
+
+
+extension ImageDownloader {
+    
+    private func validateWithETag(
+        url: URL,
+        cachedImage: UIImage,
+        cachedETag: String,
+        completion: @escaping @Sendable (DownloadResult) -> Void
+    ) {
+        dispatchQueue.async { [weak self] in
+            self?.eTagHandler.validateFetchImage(
+                with: url,
+                cachedImage: cachedImage,
+                cachedETag: cachedETag
+            ) { result in
+                switch result {
+                case .reused(let image):
+                    DispatchQueue.main.async {
+                        completion(.validated(image))
+                    }
+                case .fresh(let image):
+                    DispatchQueue.main.async {
+                        completion(.success(image))
+                    }
+                case .error(let error):
+                    DispatchQueue.main.async {
+                        completion(.failure(error))
+                    }
+                }
+            }
+        }
+    }
+    
     private func downloadAndCacheImage(
         with url: URL,
         identifier: String,
-        completion: @escaping @Sendable (UIImage?) -> Void
+        completion: @escaping @Sendable (DownloadResult) -> Void
     ) {
-        let task = session.dataTask(with: url) { [weak self] data, response, error in
-            guard let data = data,
-                  let image = UIImage(data: data),
-                  error == nil else {
+        dispatchQueue.async { [weak self] in
+            let task = self?.session.dataTask(with: url) {
+                [weak self] data,
+                response,
+                error in
+                
+                //에러처리
+                if let error = error {
+                    DispatchQueue.main.async {
+                        completion(.failure(DownloadError.networkError(error)))
+                    }
+                    return
+                }
+                
+                //데이터 유효한지?
+                guard let data = data,
+                      !data.isEmpty else {
+                    DispatchQueue.main.async {
+                        completion(.failure(DownloadError.invalidData))
+                    }
+                    return
+                }
+                
+                //이미지 유효?
+                guard let image = UIImage(data: data) else {
+                    DispatchQueue.main.async {
+                        completion(.failure(DownloadError.invalidData))
+                    }
+                    return
+                }
+                
+                var eTag: String? = nil
+                if let httpResponse = response as? HTTPURLResponse {
+                    if #available(iOS 13.0, *) {
+                        eTag = httpResponse.value(forHTTPHeaderField: "ETag")
+                    } else {
+                        eTag = (httpResponse.allHeaderFields["ETag"] as? String) ??
+                        (httpResponse.allHeaderFields["etag"] as? String)
+                    }
+                }
+                
+                guard let url = URL(string: identifier) else {
+                    DispatchQueue.main.async {
+                        completion(.success(image))
+                    }
+                    return
+                }
+                
+                let cacheable = CacheableImage(
+                    image: image,
+                    imageURL: url,
+                    identifier: identifier,
+                    eTag: eTag
+                )
+                
+                self?.cacheManager.storeImage(with: cacheable)
+                
                 DispatchQueue.main.async {
-                    completion(nil)
-                }
-                return
-            }
-            
-            if let httpResponse = response as? HTTPURLResponse {
-                let newETag: String?
-                if #available(iOS 13.0, *) {
-                    newETag = httpResponse.value(forHTTPHeaderField: "ETag")
-                } else {
-                    newETag = (httpResponse.allHeaderFields["ETag"] as? String) ??
-                    (httpResponse.allHeaderFields["etag"] as? String)
-                }
-                if let newETag = newETag {
-                    let cacheable = CacheableImage(image: image, imageURL: url, identifier: identifier, eTag: newETag)
-                    self?.cacheManager.storeImage(with: cacheable)
-                } else {
-                    let cacheable = CacheableImage(image: image, imageURL: url, identifier: identifier)
-                    self?.cacheManager.storeImage(with: cacheable)
+                    completion(.success(image))
                 }
             }
-            
-            let cacheable = CacheableImage(image: image, imageURL: url)
-            self?.cacheManager.storeImage(with: cacheable)
-            
-            DispatchQueue.main.async {
-                completion(image)
-            }
+            task?.resume()
         }
-        task.resume()
     }
-    
 }
-
--- Sources/SNKit/Policy/ETagHandler.swift
+++ Sources/SNKit/Policy/ETagHandler.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+enum ETagValidationResult {
+    case reused(UIImage) //이미지 변경 X 304
+    case fresh(UIImage) //이미지 변경
+    case error(Error)
+}
+
 final class ETagHandler: @unchecked Sendable {
     private let session: URLSession
     private let cacheManager: CacheManager
@@ -23,15 +29,24 @@ final class ETagHandler: @unchecked Sendable {
         with url: URL,
         cachedImage: UIImage,
         cachedETag: String,
-        completion: @escaping @Sendable (UIImage?) -> Void
+        completion: @escaping @Sendable (ETagValidationResult) -> Void
     ) {
         var request = URLRequest(url: url)
         request.setValue(cachedETag, forHTTPHeaderField: "If-None-Match")
         
-        let task = session.dataTask(with: request) { [weak self] data, response, error in
-            guard let httpResponse = response as? HTTPURLResponse, error == nil else {
+        let task = session.dataTask(with: request) {
+            [weak self] data,
+            response,
+            error in
+            
+            if let error = error {
+                completion(.error(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
                 DispatchQueue.main.async {
-                    completion(nil)
+                    completion(.error(DownloadError.invalidResponse))
                 }
                 return
             }
@@ -39,34 +54,16 @@ final class ETagHandler: @unchecked Sendable {
             if httpResponse.statusCode == 304 {
                 //이미지가 바뀌지 않았다면 기존의 이미지 리턴
                 DispatchQueue.main.async {
-                    completion(cachedImage)
+                    completion(.reused(cachedImage))
                 }
-            } else {
+            } else if httpResponse.statusCode == 200 {
                 //이미지가 바뀐 경우 새로 다운로드 후 리턴
-                self?.downloadAndCacheImage(with: url, completion: completion)
-            }
-        }
-        
-        task.resume()
-    }
-    
-    private func downloadAndCacheImage(
-        with url: URL,
-        completion: @escaping @Sendable (UIImage?) -> Void
-    ) {
-        //캐시 Miss -> 데이터 통신으로 이미지 로드 후 반환
-        let task = session.dataTask(with: url) { [weak self] data, response, error in
-            guard let data = data,
-                  let image = UIImage(data: data),
-                  error == nil else {
-                DispatchQueue.main.async {
-                    completion(nil)
+                guard let data = data,
+                      let image = UIImage(data: data) else {
+                    completion(.error(DownloadError.invalidData))
+                    return
                 }
-                return
-            }
-            
-            //TODO: ETag 추출 및 저장
-            if let httpResponse = response as? HTTPURLResponse {
+                
                 let newETag: String?
                 if #available(iOS 13.0, *) {
                     newETag = httpResponse.value(forHTTPHeaderField: "ETag")
@@ -74,21 +71,24 @@ final class ETagHandler: @unchecked Sendable {
                     newETag = (httpResponse.allHeaderFields["ETag"] as? String) ??
                     (httpResponse.allHeaderFields["etag"] as? String)
                 }
-                if let newETag = newETag {
-                    //TODO: 새로운 ETag 캐시매니저에 저장하기
+                
+                let identifier = url.absoluteString
+                if let newETag = newETag,
+                   let imageURL = URL(string: identifier) {
+                    let cacheable = CacheableImage(
+                        image: image,
+                        imageURL: imageURL,
+                        identifier: identifier,
+                        eTag: newETag
+                    )
+                    self?.cacheManager.storeImage(with: cacheable)
                 }
-            }
-            
-            //가져온 이미지를 캐시에 저장
-            let cacheable = CacheableImage(image: image, imageURL: url)
-            self?.cacheManager.storeImage(with: cacheable)
-            
-            DispatchQueue.main.async {
-                completion(image)
+                
+                completion(.fresh(image))
+            } else {
+                completion(.error(DownloadError.invalidResponse))
             }
         }
-        
         task.resume()
     }
-    
 }
