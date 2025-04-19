5792526c8f9e9467dc40e56008a90e9aa4544541
jeoungsung12 <jeoungsung12@naver.com>
2025-04-19 19:55:47 +0900

    Fix: Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -19,9 +17,11 @@ let package = Package(
             name: "SNKit",
             dependencies: [],
             path: "Sources",
-            publicHeadersPath: "Sources", // publicHeaderPath -> publicHeadersPath
             swiftSettings: [
                 .define("SWIFT_PACKAGE")
+            ],
+            linkerSettings: [
+                .linkedFramework("UIKit")
             ]
         ),
         .testTarget(
